734ff374396f8fefc26c297566859b378e87545f
Ashlee Hayes <hayesashlee88@gmail.com>
2020-03-02 16:33:47 -0500

    finished up to searching and sorting

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,46 @@ import AddAppointments from './AddAppointments'
 import ListAppointments from './ListAppointments'
 import SearchAppointments from './SearchAppointments'
 
+import {without} from 'lodash'
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      myAppointments: []
-    }
+      myAppointments: [],
+      formDisplay: false,
+      orderBy: 'petName',
+      orderDir: 'asc',
+      lastIndex: 0
+    };
+    this.deleteAppointment = this.deleteAppointment.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+    this.addAppointment = this.addAppointment.bind(this);
+  }
+
+toggleForm() {
+  this.setState({
+    formDisplay: !this.state.formDisplay
+  })
+}
+
+addAppointment(apt) {
+  let tempApts = this.state.myAppointments;
+  apt.aptId = this.state.lastIndex;
+  tempApts.unshift(apt);
+  this.setState({
+    myAppointments: tempApts,
+    lastIndex: this.state.lastIndex + 1,
+  })
+}
+
+  deleteAppointment(apt) {
+    let tempApts = this.state.myAppointments;
+    tempApts = without(tempApts, apt);
+
+    this.setState({
+      myAppointments: tempApts
+    })
   }
 
   componentDidMount() {
@@ -17,16 +51,34 @@ class App extends Component {
     .then(response => response.json())
     .then(result => {
       const apts = result.map(item => {
+        item.aptId = this.state.lastIndex;
+        this.setState({ lastIndex: this.state.lastIndex + 1})
         return item;
       })
       this.setState({
         myAppointments: apts
             });
     });
-
-
   }
   render() {
+
+    let order;
+    let filteredApts = this.state.myAppointments;
+    if(this.state.orderDir === 'asc') {
+      order = 1;
+          } else {
+            order = -1;
+          }
+
+          filteredApts.sort((a,b) => {
+            if (a[this.state.orderBy].toLowerCase() <
+            b[this.state.orderBy].toLowerCase()
+            ) {
+              return -1 * order;
+            } else {
+              return 1 * order
+            }
+          })
   
     return (
       <main className="page bg-white" id="petratings">
@@ -34,9 +86,17 @@ class App extends Component {
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              <AddAppointments />
+             
+              <AddAppointments
+              formDisplay={this.state.formDisplay}
+              toggleForm={this.toggleForm}
+              addAppointment = {this.addAppointment}
+              />
              <SearchAppointments />
-              <ListAppointments />
+              <ListAppointments 
+              appointments={filteredApts} 
+              deleteAppointment={this.deleteAppointment}
+              />
             </div>
           </div>
         </div>
